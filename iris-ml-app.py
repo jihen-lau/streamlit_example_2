d78b250bf383ad6d63532eb65095eb86779d6b6d
jihen-lau <111936136+jihen-lau@users.noreply.github.com>
2022-08-24 10:45:36 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -4,6 +4,7 @@ from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
+# Ji Hen First Stremlit App
 # Simple Iris Flower Prediction App
 
 This app predicts the **Iris flower** type!
