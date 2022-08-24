e83fc24a5705ec0b0ca20f98b83b817dad5f07b3
jihen-lau <111936136+jihen-lau@users.noreply.github.com>
2022-08-24 13:10:53 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -11,7 +11,7 @@ This app predicts the **Iris flower** type!
 """)
 
 from PIL import Image
-st.image("https://www.pexels.com/photo/landscape-nature-sky-man-6620743/")
+st.image("https://azkarohbiya.medium.com/iris-species-classification-using-k-neighbors-ae61cd145797/")
 
 st.sidebar.header('User Input Parameters')
 
